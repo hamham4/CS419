46b1139da954e55bc26e79b166a26d9a85272dd2
SiPLTRN <chris.siple@gmail.com>
2014-07-30 08:21:16 -0400

    Complete web scraper, data parser, and datastore
    Catalog Parser is complete. Datastore is populated. I included a couple
    extra python files I made for development of the individual pieces just
    in case you guys are interested at all.

--- parsePage/src/parsePage.py
+++ parsePage/src/parsePage.py
@@ -1,6 +1,45 @@
+import sys
+sys.path.insert(0, 'src')
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import ndb
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+from google.appengine.api import urlfetch
+import lxml
+
+BASE_URL = "http://catalog.oregonstate.edu/"
+ADD_URL = "&columns=jk"
+urlfetch.set_default_fetch_deadline(30)
+
+def make_soup(url):
+    html = urlopen(url).read()
+    return BeautifulSoup(html,"lxml")
+
+def get_dept_links(section_url):
+    soup = make_soup(section_url)
+    dlSubjects = soup.find(id = "ctl00_ContentPlaceHolder1_dlSubjects")
+    dept_links = [BASE_URL + a["href"] for a in dlSubjects.findAll("a")]
+    return dept_links
+
+def get_data(links):
+    catalog = []
+    for url in range(len(links)):
+        soup2 = make_soup(links[url]+ADD_URL)
+        table = soup2.find(id = "ctl00_ContentPlaceHolder1_dlCourses")
+        for div in table.findAll('div'):
+            data = div.findAll('td')
+            name = data[0]
+            sched = data[1]
+            for br in sched.findAll('br'):
+                br.replace_with(" ")
+            #strips out Staff and TBA entries from catalog 
+            if name.get_text() == "Staff" or sched.get_text().strip() == "TBA":
+                continue
+            entry = name.get_text().strip()+" "+sched.get_text().strip()
+            catalog.append(entry)
+    return catalog
+
 
 class catdb(ndb.Model):
     user = ndb.StringProperty(required=True)
@@ -18,65 +57,103 @@ class catdb(ndb.Model):
 
 class MainPage(webapp.RequestHandler):
     
-    
     def get(self):
-        data = "Siple, C. MWF 0700-0800 6/26/14-9/6/14"
-        cnt = 0
-        last = ""
-        first = ""
-        days = ""
-        stime = ""
-        etime = ""
-        sdate = ""
-        edate = ""
-        usr = ""
+        
 
-        for i in range(len(data)):
-            mon = False
-            tues = False
-            wed = False
-            thur = False
-            fri = False
-            
-            if data[i] == " " or data[i] == "-" or data[i] == "." or data[i] == ",":
-                cnt += 1
-                continue
-            if cnt == 0:
-                last += data[i]
-            if cnt == 2:
-                first += data[i]
-            if cnt == 4:
-                days += data[i]
-            if cnt == 5:
-                stime += data[i]
-            if cnt == 6:
-                etime += data[i]
-            if cnt == 7:
-                sdate += data[i]
-            if cnt == 8:
-                edate += data[i]
+        
+        links = get_dept_links("http://catalog.oregonstate.edu/SOC.aspx?level=all&campus=corvallis&term=201500")
+        catData = get_data(links)
+        
+        for k in range(len(catData)):
+            cnt = 0
+            last = ""
+            first = ""
+            days = ""
+            stime = ""
+            etime = ""
+            sdate = ""
+            edate = ""
+            usr = ""
+            data = catData[k]
+            for i in range(len(data)):
+                mon = False
+                tues = False
+                wed = False
+                thur = False
+                fri = False
                 
-        for j in range(len(last)):
-            if j == 7:
-                break
-            usr += last[j].lower()
-    
-        usr += first.lower()
-            
-        for n in range(len(days)):
-            if days[n] == "M":
-                mon = True
-            if days[n] == "T":
-                tues = True
-            if days[n] == "W":
-                wed = True
-            if days[n] == "R":
-                thur = True
-            if days[n] == "F":
-                fri = True
-                                    
-        cat = catdb(user=usr, lname=last, fi=first, M=mon, T=tues, W=wed, R=thur, F=fri, st=stime, et=etime, sd=sdate, ed=edate)
-        cat.put()
+                if data[i] == " " or data[i] == "-" or data[i] == "." or data[i] == ",":
+                    cnt += 1
+                    continue
+                if cnt == 0:
+                    last += data[i]
+                if cnt == 2:
+                    first += data[i]
+                if cnt == 4:
+                    days += data[i]
+                if cnt == 5:
+                    stime += data[i]
+                if cnt == 6:
+                    etime += data[i]
+                if cnt == 7:
+                    sdate += data[i]
+                if cnt == 8:
+                    edate += data[i]
+                if cnt ==9:
+                    usr = ""
+                    for j in range(len(last)):
+                        if j==7:
+                            break
+                        usr += last[j].lower()
+                    usr += first.lower()
+                    
+                    for n in range(len(days)):
+                        if days[n] == "M":
+                            mon = True
+                        if days[n] == "T":
+                            tues = True
+                        if days[n] == "W":
+                            wed = True
+                        if days[n] == "R":
+                            thur = True
+                        if days[n] == "F":
+                            fri = True
+                    
+                    
+                    cat1 = catdb(user=usr, lname=last, fi=first, M=mon, T=tues, W=wed, R=thur, F=fri, st=stime, et=etime, sd=sdate, ed=edate)
+                    cat1.put()
+                    days = ""
+                    stime = ""
+                    etime = ""
+                    sdate = ""
+                    edate = ""
+                    days += data[i]
+                    cnt = 4
+                    continue
+        
+            usr = ""
+                    
+            for j in range(len(last)):
+                if j == 7:
+                    break
+                usr += last[j].lower()
+        
+            usr += first.lower()
+                
+            for n in range(len(days)):
+                if days[n] == "M":
+                    mon = True
+                if days[n] == "T":
+                    tues = True
+                if days[n] == "W":
+                    wed = True
+                if days[n] == "R":
+                    thur = True
+                if days[n] == "F":
+                    fri = True
+                                        
+            cat2 = catdb(user=usr, lname=last, fi=first, M=mon, T=tues, W=wed, R=thur, F=fri, st=stime, et=etime, sd=sdate, ed=edate)
+            cat2.put()
         
         
         self.response.headers['Content-Type'] = 'text/plain'
---
+++ stringify.py
@@ -0,0 +1,94 @@
+data = "Siple, C. MF 0700-0800 6/26/14-9/6/14 TR 1300-1400 6/26/14-9/6/14 W 1100-1150 6/26/14-9/6/14"
+
+cnt = 0
+last = ""
+first = ""
+days = ""
+stime = ""
+etime = ""
+sdate = ""
+edate = ""
+usr = ""
+	
+
+for i in range(len(data)):
+	mon = False
+	tues = False
+	wed = False
+	thur = False
+	fri = False
+	
+	if data[i] == " " or data[i] == "-" or data[i] == "." or data[i] == ",":
+		cnt += 1
+		continue
+	if cnt == 0:
+		last += data[i]
+	if cnt == 2:
+		first += data[i]
+	if cnt == 4:
+		days += data[i]
+	if cnt == 5:
+		stime += data[i]
+	if cnt == 6:
+		etime += data[i]
+	if cnt == 7:
+		sdate += data[i]
+	if cnt == 8:
+		edate += data[i]
+	if cnt == 9:
+		usr = ""
+		for j in range(len(last)):
+			if j == 7:
+				break
+			usr += last[j].lower()  
+		usr += first.lower()
+				
+		for n in range(len(days)):
+			if days[n] == "M":
+				mon = True
+			if days[n] == "T":
+				tues = True
+			if days[n] == "W":
+				wed = True
+			if days[n] == "R":
+				thur = True
+			if days[n] == "F":
+				fri = True
+				
+		print (usr, last, first, days, stime, etime, sdate, edate)
+		days = ""
+		stime = ""
+		etime = ""
+		sdate = ""
+		edate = ""
+		days += data[i]
+		cnt = 4
+		continue
+		
+usr = ""			
+                
+for j in range(len(last)):
+	if j == 7:
+		break
+	usr += last[j].lower()
+    
+usr += first.lower()
+            
+for n in range(len(days)):
+	if days[n] == "M":
+		mon = True
+	if days[n] == "T":
+		tues = True
+	if days[n] == "W":
+		wed = True
+	if days[n] == "R":
+		thur = True
+	if days[n] == "F":
+		fri = True
+		
+print (usr, last, first, days, stime, etime, sdate, edate)
+		
+		
+		
+		
+		
