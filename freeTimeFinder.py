2db4705bdb6e5565353c95c220179d4bbb300031
Sara Thorup <thorups@gmail.com>
2014-08-10 10:34:38 -0700

    added comments

--- freeTimeFinder.py
+++ freeTimeFinder.py
@@ -7,11 +7,15 @@ BusyBlock = namedtuple("BusyBlock", "year, month, day, startTime endTime")
 FreeBlock = namedtuple("FreeBlock", "year, month, day, startTime, endTime")
 
 def main():
-
+	# sample busy times stored in a list
 	b1 = BusyBlock("2014", "8", "3", "9:00am", "11:00am")
 	b2 = BusyBlock("2014", "8", "3", "1:00pm", "5:00pm")
 	busyTimes = [b1, b2]
+
+	#list of free times to be returned
 	freeTimes = list()
+	
+	#create free time windows based on busy times
 	stop = "12:00am"
 	busyYear, busyMonth, busyDay, busyStart, busyStop = busyTimes[0]
 	for busyTime in busyTimes:
@@ -21,11 +25,12 @@ def main():
 		stop = busyStop
 		freeTime = FreeBlock(busyYear, busyMonth, busyDay, freeStart, freeStop)
 		freeTimes.append(freeTime)
-
+	#The last free time window
 	freeTime = FreeBlock(busyYear, busyMonth, busyDay, stop, "11:59pm")
+	
 	freeTimes.append(freeTime)
 	
-	
+	#display free times
 	for time in freeTimes:
 		print time
 main()
