3076e6231d3dc07f8298ba2730ce077ec2b993b6
Sara Thorup <thorups@gmail.com>
2014-08-17 20:51:45 -0700

    fixed program to change busy to free times

--- freeTimeFinder.py
+++ freeTimeFinder.py
@@ -1,36 +1,39 @@
 #The purpose of this program is to return a list of times that an individual is free
-#Params: The times the the individual is busy for a certain day
-
+#Params: A list of named tuples of busy times
 from collections import namedtuple
 
 BusyBlock = namedtuple("BusyBlock", "year, month, day, startTime endTime")
 FreeBlock = namedtuple("FreeBlock", "year, month, day, startTime, endTime")
 
-def main():
-	# sample busy times stored in a list
-	b1 = BusyBlock("2014", "8", "3", "9:00am", "11:00am")
-	b2 = BusyBlock("2014", "8", "3", "1:00pm", "5:00pm")
-	busyTimes = [b1, b2]
-
+def getFreeTimesList(busyBlockList):
 	#list of free times to be returned
 	freeTimes = list()
-	
-	#create free time windows based on busy times
-	stop = "12:00am"
-	busyYear, busyMonth, busyDay, busyStart, busyStop = busyTimes[0]
-	for busyTime in busyTimes:
-		busyYear, busyMonth, busyDay, busyStart, busyStop = busyTime
-		freeStart = stop
-		freeStop = busyStart
-		stop = busyStop
-		freeTime = FreeBlock(busyYear, busyMonth, busyDay, freeStart, freeStop)
-		freeTimes.append(freeTime)
+
+	#Inital end of busy time
+	endOfBusyTime = "12:00am"
+
+	#Create a list of free time blocks for every busy block
+
+	#set the busy variables to the first BusyBlock in hte list
+	for busyWindow in busyBlockList:
+		busyYear, busyMonth, busyDay, busyStartTime, busyStopTime = busyWindow
+		freeStartTime = endOfBusyTime
+		freeStopTime = busyStartTime
+		endOfBusyTime = busyStopTime
+
+		freeBlock = FreeBlock(busyYear, busyMonth, busyDay, freeStartTime, freeStopTime)
+		freeTimes.append(freeBlock)
+
+	#End of free time block 
+	endOfFreeTime = "11:59pm"
+
+	#Get the year, month, and day for the time block
+	busyYear, busyMonth, busyDay, busyStartTime, busyStopTime = busyBlockList[0]
+
 	#The last free time window
-	freeTime = FreeBlock(busyYear, busyMonth, busyDay, stop, "11:59pm")
-	
-	freeTimes.append(freeTime)
-	
-	#display free times
-	for time in freeTimes:
-		print time
-main()
+	freeBlock = FreeBlock(busyYear, busyMonth, busyDay, endOfBusyTime, endOfFreeTime)
+
+	#Add the final free block to the list
+	freeTimes.append(freeBlock)
+
+	return freeTimes
