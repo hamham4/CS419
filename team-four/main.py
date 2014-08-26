51bcf2154e7e320006209021eef2ac2b7747cb97
Sara Thorup <thorups@gmail.com>
2014-08-26 14:27:33 -0700

    attempted to get busy times from DB

--- team-four/main.py
+++ team-four/main.py
@@ -15,20 +15,35 @@
 # limitations under the License.
 #
 import webapp2
-import jsonManipulator
+from collections import namedtuple
+import jsonManipulator 
+import busy_times_db
+
+BusyBlock = namedtuple("BusyBlock", "year, month, day, startTime endTime")
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
 
 class SubmissionHandler(webapp2.RequestHandler):
-
 	def post(self):
-		#Get request from Post
+		#Get request in json format from Post
 		jsonParameters = self.request.get('request', self)
-
 		#Parse out parameters from json
 		requestType, startYear, endYear, startMonth, endMonth, startDay, endDay, startTime, endTime, attendees = jsonManipulator.getParsedParameters(jsonParameters)
+		busyTimes = busy_times_db.busy_times_db("17", "06", "2014", "driskilq" )
+		self.response.write(busyTimes)
+		self.response.write("boo")
+
+		# for attendee in attendees:
+		# 	for year in range(int(startYear), int(endYear)+ 1):
+		# 		for month in range(int(startMonth), int(endMonth) + 1):
+		# 			for day in range(int(startDay), int(endDay) + 1):
+		# 				busyTimes = busy_times_db.busy_times_db(day, month, year, attendee)
+		# 				self.response.write(busyTimes)
+
+
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/submit', SubmissionHandler)
