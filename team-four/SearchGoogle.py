94bb86b156a9993b1d7c3b348a49a364e8d65fd3
Sara Thorup <thorups@gmail.com>
2014-09-04 21:50:52 -0700

    removed comments

--- team-four/SearchGoogle.py
+++ team-four/SearchGoogle.py
@@ -31,24 +31,6 @@ from google.appengine.api import urlfetch
 
 BusyBlock = namedtuple("BusyBlock", "year, month, day, startTime endTime")
 
-# CLIENT_SECRETS = os.path.join(os.path.dirname(__file__), 'client_secrets.json')
-
-# FLOW = client.flow_from_clientsecrets(CLIENT_SECRETS,
-#   scope=[
-#       'https://www.googleapis.com/auth/calendar',
-#       'https://www.googleapis.com/auth/calendar.readonly',
-#     ],
-#     message=tools.message_if_missing(CLIENT_SECRETS))
-
-# FLAGS = gflags.FLAGS
-# FLOW = OAuth2WebServerFlow(
-    # client_id='350349023880-2f831jg4i6m8ee5h9f7jp742cmqm8efc.apps.googleusercontent.com',
-    # client_secret='ZpguubUd9SEH9pkVvl-QPi8G',
-     # scope=[
-      # 'https://www.googleapis.com/auth/calendar',
-      # 'https://www.googleapis.com/auth/calendar.readonly',
-    # ],
-    # user_agent='team-four/1')
 
 
 def getCalendarEvents(userID, startYear, startMonth, startDay, endYear, endMonth, endDay):
@@ -72,21 +54,8 @@ def getCalendarEvents(userID, startYear, startMonth, startDay, endYear, endMonth
 
   return googleSearch(userID, startTimeParam, startDate, endTime, endDate)
 
-#def googleSearch(userId, startYear, endYear, startMonth, endMonth, startday, endDay, startTime, endTime):
+
 def googleSearch(userId, startTimeParam, startDate, endTime, endDate):
-  #used from the google reference code
-  # storage = Storage('calendar.dat')
-  # credentials = storage.get()
-  # if credentials is None or credentials.invalid:
-    # pass
-    ##print credentials
-    #credentials = run(FLOW, storage)
-
-  # Create an httplib2.Http object to handle our HTTP requests and authorize it with our good Credentials.
-  #http = httplib2.Http()
-  #http = credentials.authorize(http)
-
-  # Construct the service object for the interacting with the Calendar API.
 
   service = build(serviceName='calendar', version='v3',
        developerKey='AIzaSyCInh7DEEH7Zv2H-htNy7o9Z_7ktqkWY1Q')
@@ -202,104 +171,13 @@ def googleSearch(userId, startTimeParam, startDate, endTime, endDate):
 		#order by the start time, have it all as single events, min and max time are our parameters we put in
 		#also we are having it return in the time zone of the local machine, not useful for full day events but very helpful for datetime events
       events = service.events().list(calendarId=calendarID, pageToken=page_token, orderBy=startTime, singleEvents=True, timeMin=myStartTime, timeMax=myEndTime).execute()
-      #print events
-      #except:
-	    #when we get an error from the events return, normally meaning a bad onid id
-        #events = "NoID"
-		
-      #def ld_writeDicts(events):
-        #f=open(filePath, 'w')
-      # newData = json.dumps(events)
 
-      # #ld_writeDicts(events)
-
-      # #busyTimes = list()
-
-	  # #json_data = json.loads(newData)
-      # #with json.loads(newData) as json_file:
-      # json_data = json.loads(newData)
-
-      # i = -1
-      # for items in json_data['items']:
-            # n = i
-            # i +=1
-            # for key, value in items.iteritems():
-                # start = json_data['items'][n]['start']
-                # start1 = str(start)
-                # if len(start1) == 43:
-                    # st1 = start1[16:32]
-                    # year = st1[0:4]
-                    # month = st1[5:7]
-                    # day = st1[8:10]
-                    # sHour = st1[11:13]
-                    # sMin =  st1[14:16]
-                    # startTime = sHour + sMin
-                # if len(start1) == 24:
-                    # st1 = start1[12:22]
-                    # year = st1[0:4]
-                    # month = st1[5:7]
-                    # day = st1[8:10]
-                    # #sHour = st1[12:13]
-                    # #sMin =  st1[14:15]
-                    # startTime = "0000"
-                # end = json_data['items'][n]['end']
-                # end1 = str(end)
-                # if len(end1) == 43:
-                    # en1 = end1[16:32]
-                    # eYear = en1[0:4]
-                    # eMonth = en1[5:7]
-                    # eDay = en1[8:10]
-                    # eHour = en1[11:13]
-                    # eMin =  en1[14:16]
-                    # endTime = eHour + eMin
-                # if len(end1) == 24:
-                    # en1 = end1[12:28]
-                    # eYear = en1[0:4]
-                    # eMonth = en1[5:7]
-                    # eDay = en1[8:10]
-                    # #eHour = en1[11:13]
-                    # #eMin =  en1[14:16]
-                    # endTime = "0000"
-
-            #print year, month, day, startTime, eYear, eYear, eMonth, eDay, endTime
-
-            #busyBlock = BusyBlock(year, month, day, startTime, endTime)
-            #busyTimes.append(busyBlock)
-        #return busyTimes
 
       if not page_token:
         break
 
   except client.AccessTokenRefreshError:
     pass
-    #print ("The credentials have been revoked or expired, please re-run the application to re-authorize")
 
-  #return events
   
-  
-if __name__ == '__main__':
-  userId = "rezalution786"
 
-  startYear = "2014"
-  startMonth = "08"
-  startDay =  "30"
-  startDate = startYear + "-" + startMonth + "-" + startDay
-  startDate = str(startDate)
-
-  startHour = "00"
-  startMin =  "00"
-  startTimeParam = startHour + ":" + startMin
-  startTimeParam = str(startTimeParam)
-
-  endHour = "00"
-  endMin = "00"
-  endTime = endHour + ":" + endMin
-  endTime = str(endTime)
-
-  endYear = "2014"
-  endMonth = "09"
-  endDay =  "02"
-  endDate = endYear + "-" + endMonth + "-" + endDay
-  endDate = str(endDate)
-
-  googleSearch(userId, startTimeParam, startDate, endTime, endDate)
